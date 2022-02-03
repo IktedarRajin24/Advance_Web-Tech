dee2b13fec2267ddea7bde23a78f3322c2b23746
Rajin, MD. Iktedar Hasan Rushdi <rushdi2410@gmail.com>
2022-02-03 18:29:16 +0600

    first lab task

---
+++ APWT-TASK-1/resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+<html>
+    <body>
+        @section('content')
+        <br>
+        <br>
+        <br>
+            <h1 style="background-color: skyblue">Hello, This is our home page.</h1>
+            <h4 style="background-color: lightgreen">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloribus esse ex beatae quos, necessitatibus quia optio veritatis omnis illum, quidem nobis libero consequuntur pariatur eum vel? Ratione unde recusandae ad laudantium quaerat reiciendis officiis? Totam praesentium temporibus aspernatur similique. Repellat neque repudiandae temporibus. Perspiciatis esse aut omnis aspernatur, officia quod minus consequatur nesciunt saepe ipsa repellendus fugit at tempora quos laudantium eveniet assumenda repudiandae excepturi deserunt aliquid suscipit sequi. Omnis natus consequatur dolorum quisquam corrupti vitae, necessitatibus inventore tempore error alias hic dolorem facere molestias amet ducimus minima nesciunt suscipit repudiandae quaerat quos officiis odit voluptatem quia. Rerum eligendi vel reiciendis excepturi quam numquam quisquam illo nihil, officiis incidunt fugiat aliquid recusandae minus ad sequi, consequatur ut exercitationem molestiae. Cumque, fuga! Dolorum, sequi officiis facere fuga velit ut. Accusantium nam obcaecati dicta vitae veritatis reiciendis iste doloribus eligendi? Eveniet quam nemo saepe praesentium hic! Iusto fugit animi, maxime, inventore sunt vitae iure porro beatae expedita esse aliquid est, architecto deleniti. Ut error eos esse et, quibusdam ullam accusamus accusantium est, asperiores dolorum officia cum quidem ducimus ad nemo saepe? Veritatis facere, illum optio commodi qui, ab nisi, quis fugiat quas necessitatibus eligendi possimus dolorum sit deserunt aperiam voluptates. Cupiditate.
+            </h4>
+        @endsection
+
+    </body>
+
+</html>
---
+++ APWT-TASK-1/resources/views/inc/topnav.blade.php
@@ -0,0 +1,11 @@
+<div style="background-color: skyblue">
+    <a href="{{'home'}}" class="btn btn-primary">Home</a>
+
+    <a href="{{'products'}}" class="btn btn-primary">Products</a>
+
+    <a href="{{'ourTeam'}}" class="btn btn-primary">Our Team</a>
+
+    <a href="{{'contactUs'}}" class="btn btn-primary">Contact us</a>
+
+    <a href="{{'aboutUs'}}" class="btn btn-primary">About us</a>
+</div>
